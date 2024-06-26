267588d12dcf73607d463a39e5b870ad86fbd369
maurice <mauricemiema90@gmail.com>
2024-06-26 20:17:00 +0200

    responsive terminer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["./index.html", "./scrypt.js"],
   theme: {
     extend: {
+      fontFamily: {
+        primary: "Poppins",
+      },
+
       screens : {
         'petitecran' : {'max': '380px'},
-      }
+      },
+
+      
     },
   },
   plugins: [],
